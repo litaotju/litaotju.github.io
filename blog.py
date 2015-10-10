6fc97bdc7d3e63a6524af3247885c57a45c3db44
litao <litaotju@live.cn>
2015-10-10 16:09:55 +0800

    将添加博客头函数模块化

--- blog.py
+++ blog.py
@@ -9,31 +9,30 @@ def blog(title):
     format_time = datetime.datetime.now().strftime("%Y-%m-%d-")
     whole_title = format_time + title
     filename = "_posts\\%s.md" % whole_title 
+
     if os.path.exists(filename):
         print "Warning:Has same blog already"
-##        override = True if raw_input("Override old one(y/n)?:")in 'yY' else False
-##        if not override:
-##            print "No file been created"
-##            sys.exit()
+       # override = True if raw_input("Override old one(y/n)?:")in 'yY' else False
+       # if not override:
+        #    print "No file been created"
+        #    sys.exit()
         sys.exit()
     try:
         blog_file = open(filename, 'w')
     except Exception,e:
         print e
         traceback.print_exc()
-    blog_file.write('''---
-layout: post
-title: "%s"
-description: 
-category:
-tags: 
----
-{%% include JB/setup %%}
-''' % title)
+    blog_file.write( blog_head(title) )
     print "%s created successfully" % filename
     blog_file.close()
     return filename
 
+def blog_head(title, layout='post', desc = '', cate = '',  tags = ''):
+    "博客头的模板"
+    standard_head = '''---\nlayout: %s\ntitle: "%s"\ndescription: %s\ncategory: %s \ntags: %s\n---\n{%% include JB/setup %%}'''\
+         % (layout, title, desc, cate, tags)
+    return standard_head
+    
 if __name__ == "__main__":
     title = raw_input("plz enter blog_title:")
     filename = blog(title)
